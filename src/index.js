26817eaaefd648eced55c6a6e6bc164e323457f5
Martin Banks <anotherbanksy@gmail.com>
2018-08-03 16:18:54 +1000

    fixes for basic working version

--- src/index.js
+++ src/index.js
@@ -10,39 +10,41 @@
 // </div>
 // </div>`
 
+let lastCalledTime = 1
+// let fpsValue = 1
+let delta = 1
+let fpsCounter = 0
+const fpsHistory = [... new Array(100)].map(() => 60)
+
+
 const fpsmeter = document.createElement('div')
 fpsmeter.setAttribute('id', 'fpsMeterContainer')
 fpsmeter.setAttribute('class', 'fps')
 
 const fps = document.createElement('p')
-fps.setAtrribute('class', 'fps__value')
+fps.setAttribute('class', 'fps__value')
 
-const fpsHistory = document.createElement('div')
-fpsHistory.setAttribute('class', 'fps__history')
+const fpsHistoryContainer = document.createElement('div')
+fpsHistoryContainer.setAttribute('class', 'fps__history')
 
 const ticks = fpsHistory.map((t, i) => {
   const newTick = document.createElement('div')
   newTick.setAttribute('id', `tick-${`000${i}`.slice(-3)}`)
-  newTick.setAttribute('class', 'fps__meter--tick')
+  newTick.setAttribute('class', 'fps__history--tick')
   return newTick
 })
 
 ticks.forEach(t => {
-  fpsHistory.appendChild(t)
+  fpsHistoryContainer.appendChild(t)
 })
 
 fpsmeter.appendChild(fps)
-fpsmeter.appendChild(fpsHistory)
+fpsmeter.appendChild(fpsHistoryContainer)
 document.body.appendChild(fpsmeter)
 
-const domTicks = [... document.querySelectorAll('.tick')]
+const domTicks = [... document.querySelectorAll('.fps__history--tick')]
 const domFpsValue = document.querySelector('.fps__value')
 
-let lastCalledTime = 1
-// let fpsValue = 1
-let delta = 1
-let fpsCounter = 60
-const fpsHistory = [... new Array(100)].map(() => 60)
 
 function update () {
   if (!lastCalledTime) {
@@ -57,16 +59,17 @@ function update () {
     const fpsValue = Math.min(1 / delta, 60)
     const fpsAverage = Math.round(fpsHistory.reduce((a, b) => a + b, 0) / 100)
     domFpsValue.innerText = `${Math.round(fpsValue)}fps | Average: ${fpsAverage}`
-    const color = opacity => `hsla(${(Math.max(fpsVal - 10, 0) / 60) * 150}, 100%, 50%, ${opacity})`
-    domTicks.forEach(t => {
-      t.style.height = `${fpsVal / 60 * 100}%`
+    const color = opacity => `hsla(${(Math.max(fpsValue - 10, 0) / 60) * 150}, 100%, 50%, ${opacity})`
+    console.log('frame')
+    domTicks.forEach((t, i) => {
+      t.style.height = `${fpsHistory[i] / 60 * 100}%`
       t.style.background = color(0.4)
       t.style.borderTop = `solid 3px ${color(1)}`
     })
     fpsCounter = 0
   }
-  fpsValueHistory.push(Math.min(1 / delta, 60))
-  if (fpsValueHistory.length > 100) fpsValueHistory.shift()
+  fpsHistory.push(Math.min(1 / delta, 60))
+  if (fpsHistory.length > 100) fpsHistory.shift()
   fpsCounter++
   window.requestAnimationFrame(update)
 }
